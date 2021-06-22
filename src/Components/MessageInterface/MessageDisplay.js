2d796f38511ff043a89a4a099f723c6f0c9a9757
Sam Hall <sam-hall@lambdastudents.com>
2021-06-21 20:04:55 -0500

    added some logs to track app progress through connection and messaging. Edited MessageDisplay to include a subcomponent that is conditionally rendered, rather than conditionally rendering a block, which was slower for some reason.

--- src/Components/MessageInterface/MessageDisplay.js
+++ src/Components/MessageInterface/MessageDisplay.js
@@ -11,16 +11,21 @@ const MessageDisplay = ({ messageStream }) => {
   }, [messageStream]);
 
   return messagesToDisplay ? (
+    <ShowMessages messageStream={messageStream} />
+  ) : (
+    <p>Nothing to display</p>
+  );
+};
+
+// not sure why, but this way is much faster than putting the contents in the ternary
+const ShowMessages = ({ messageStream }) => {
+  return (
     <div className="flex align-bottom overflow-auto">
       <ul className="flex flex-col w-full max-w-full">
         {messageStream.map((incomingMsg) => (
           <Message msg={incomingMsg.message} from={incomingMsg.from} />
         ))}
       </ul>
-    </div>
-  ) : (
-    <div>
-      <p>Nothing to display</p>
     </div>
   );
 };
