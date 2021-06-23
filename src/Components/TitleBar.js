ec952465838feefaf9befff16504fe3b1c324fb6
Sam Hall <sam-hall@lambdastudents.com>
2021-06-23 08:27:34 -0500

    added link to '/' on title in tilebar

--- src/Components/TitleBar.js
+++ src/Components/TitleBar.js
@@ -4,9 +4,11 @@ const TitleBar = () => {
   return (
     <div className="flex flex-row justify-between fixed border-2 border-purple-700 w-full bg-purple-700">
       <p className="invisible">Login</p>
-      <h1 className="text-white font-lobster self-center justify-self-center text-center text-2xl">
-        Meet
-      </h1>
+      <Link to="/">
+        <h1 className="text-white font-lobster self-center justify-self-center text-center text-2xl">
+          Meet
+        </h1>
+      </Link>
       <nav className="border-2 border-red-600">
         <Link className="" to="/login">
           Login
